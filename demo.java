6d7687e9ff8d5bb90d5efc2f0a502c8e4368a2ad
ShriomRana <shriitedu7830@gmail.com>
2023-06-14 08:46:15 +0530

    second commit

--- demo.java
+++ demo.java
@@ -1,8 +1,24 @@
 import java.util.*;
-class demo 
+class programer
 {
-public static void main(String[]args)
+    public void user()
+    {
+        System.out.println("wright the program");
+    }
+}
+class demo extends programer
+{
+public void coder()
 {
 System.out.println("hello programer");
 }
 }
+class main {
+ public static void main(String[]args){
+demo shri = new demo();
+shri.coder();
+programer c = new programer();
+c.user();
+}
+}
+
